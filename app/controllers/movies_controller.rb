1360e86e46d947949f229b40c244605c0db5cada
Will Conry <willconrydeveloper@gmail.com>
2019-07-02 00:52:35 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,14 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    sorting = params[:sort] # retrieve sorting method from URI
+    if sorting == 'title'
+      @movies = Movie.order(:title)
+    elsif sorting == 'date'
+      @movies = Movie.order(:release_date)
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
